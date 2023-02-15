5801a020c483a11e7a366b6aadac05473ce0cb72
ChoYeSeul <jys0323117@naver.com>
2023-02-15 14:46:05 +0900

    feat: publish 설정

--- more-study/src/App.js
+++ more-study/src/App.js
@@ -1,13 +1,18 @@
+// gh-pages download -> npm run deploy 명령어 입력 후 publish
+// 빈 화면이 publish된 경우 path에 process.env.PUBLIC_URL 추가
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Reset } from "styled-reset";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <Router>
+      <Reset />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:movieId" element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/movie/:movieId`} element={<Detail />} />
       </Routes>
     </Router>
   );
